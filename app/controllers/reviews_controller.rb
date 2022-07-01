1b13bb1efaed532ab7d24c0912ae146da13544a0
shun19970310 <shunma19970310@gmail.com>
2022-07-01 02:03:58 +0000

    [Add]reviewコントローラ

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,21 @@
 class ReviewsController < ApplicationController
   def index
   end
+
+  def create
+    @review = Review.new(review_params)
+    @review.user_id = current_user.id
+    if @review.save
+      redirect_to ramen_shop_reviews_path(@review.ramen_shop)
+    else
+      @ramen_shop = RamenShop.find(params[:id])
+      # ramen_shopsコントローラに定義されている@ramen_shop = RamenShop.find(params[:id])をcreateアクション内に記述する必要がある（renderを使うとき）
+      render "ramen_shops/show"
+    end
+  end
+
+  private
+  def review_params
+    params.require(:review).permit(:ramen_shop_id, :score, :content)
+  end
 end
