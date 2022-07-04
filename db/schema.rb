08a1f91de921c97756e1a52235b6d8cf3885aea4
shun19970310 <shunma19970310@gmail.com>
2022-07-04 09:52:26 +0000

    [Add]relationshipsモデル

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_29_093008) do
+ActiveRecord::Schema.define(version: 2022_07_04_095106) do
 
   create_table "installs", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -26,6 +26,13 @@ ActiveRecord::Schema.define(version: 2022_06_29_093008) do
 
   create_table "ramen_shops", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer "following_id", null: false
+    t.integer "follower_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
