d048d8ac77373fde2583a32226dca06982208f2d
shun19970310 <shunma19970310@gmail.com>
2022-07-05 14:28:46 +0000

    [Add]ルーティング

---
+++ app/controllers/relationships_controller.rb
@@ -0,0 +1,8 @@
+class RelationshipsController < ApplicationController
+  # コントローラ作成時に--skip-template-engineを付けることでviewの作成を省略できる
+  def create
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,10 @@ Rails.application.routes.draw do
   resources :ramen_shops, only: [:index, :show] do
     resources :reviews, only: [:index, :create]
   end
+  resources :users do
+    resource :relationships, only: [:create, :destroy]
+    get :followings, on: :member
+    get :followers, on: :member
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
