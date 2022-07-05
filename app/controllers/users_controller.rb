a7559f629ec0cbf7d5add8fafaebfa89e763e0bd
shun19970310 <shunma19970310@gmail.com>
2022-07-05 14:40:15 +0000

    [Add]usersコントローラ

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user!
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def index
+    # idがcurrent_user.id以外のuser全てを取得
+    @users = User.where.not(id: current_user.id)
+  end
+  
+  def followings
+    user = User.find(params[:id])
+    @users = user.followings
+  end
+  
+  def followers
+    user = User.find(params[:id])
+    @users = user.followers
+  end
+end
