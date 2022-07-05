f1d789c379443ca9b90a68da365ba29cbff425a8
shun19970310 <shunma19970310@gmail.com>
2022-07-05 14:34:11 +0000

    [Add]relationshipsコントローラ

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,8 +1,15 @@
 class RelationshipsController < ApplicationController
+  before_action :authenticate_user!
   # コントローラ作成時に--skip-template-engineを付けることでviewの作成を省略できる
   def create
+    following = current_user.relationships.build(follower_id: params[:user_id])
+    following.save
+    redirect_to request.referrer || root_path
   end
 
   def destroy
+    following = current_user.relationships.find_by(follower_id: params[:user_id])
+    following.destroy
+    redirect_to request.referrer || root_path
   end
 end
