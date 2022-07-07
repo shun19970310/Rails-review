caf79221f5f21cf06a4e00dbd2e62fc7ed40972e
shun19970310 <shunma19970310@gmail.com>
2022-07-07 05:59:19 +0000

    [Add]users/show.html

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,8 @@ class User < ApplicationRecord
 
   has_many :reverse_of_relationships, class_name: "Relationships", foreign_key: :follower_id
   has_many :followers, through: :reverse_of_relationships, source: :following
+  # あるユーザーが引数のuserにフォローされているか否かを判定するメソッド
+  def is_followed_by?(user)
+    reverse_of_relationships.find_by(following_id: user.id).present?
+  end
 end
